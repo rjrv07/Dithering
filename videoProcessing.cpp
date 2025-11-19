014d92997c4e3b5c9d18ced9cf03ac8f6979e335
Roelie03 <109596367+Roelie03@users.noreply.github.com>
2025-11-19 16:59:13 +0200

    Add video processing

---
+++ videoProcessing.cpp
@@ -0,0 +1,48 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+constexpr std::string inputVideoPrefix = "../input_videos/";
+constexpr std::string outputVideoPrefix = "../output_videos/";
+constexpr std::string palettePrefix = "../palettes/";
+
+int main() {
+    // Open the video file
+    VideoCapture cap(".mp4");
+
+    // Check if the video was opened successfully
+    if (!cap.isOpened()) {
+        std::cout << "Error: Could not open video file." << std::endl;
+        return -1;
+    }
+
+    // Get frame width and height
+    int frame_width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
+    int frame_height = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
+
+    // Define the codec and create VideoWriter object
+    VideoWriter out("output.avi",
+        VideoWriter::fourcc('m', 'p', '4', 'v'),
+    cap.get(CAP_PROP_FPS),
+        Size(frame_width, frame_height)
+    );
+
+    Mat frame;
+    while (cap.read(frame)) {
+        // Write the frame to the output video file
+        out.write(frame);
+
+        // Display the frame
+        imshow("Frame", frame);
+        if (waitKey(1) == 'q') {
+            break;
+        }
+    }
+
+    // Release everything
+    cap.release();
+    out.release();
+    destroyAllWindows();
+    return 0;
+}
